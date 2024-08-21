84d6de1f5d3b5ebd17656198328a5a79ea489331
yoonsun124 <dbstjs0124@gmail.com>
2024-08-21 23:56:00 +0900

    first add

---
+++ src/main/java/com/instaTest/BackEnd_InstaTest/BackEndInstaTestApplication.java
@@ -0,0 +1,13 @@
+package com.instaTest.BackEnd_InstaTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndInstaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndInstaTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/instaTest/BackEnd_InstaTest/BackEndInstaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.instaTest.BackEnd_InstaTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndInstaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
